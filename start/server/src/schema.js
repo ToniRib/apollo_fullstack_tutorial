3cc4c6e66f3967f7b1b339c8572a9ca476a08a6e
Toni Rib <toni.rib@gusto.com>
2019-07-27 12:40:41 -0600

    Add additional resolvers

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -65,7 +65,7 @@ const typeDefs = gql`
 
     type Mission {
         name: String
-        missionPatch(size: PatchSize): String
+        missionPatch(mission: String, size: PatchSize): String
     }
 
     enum PatchSize {
